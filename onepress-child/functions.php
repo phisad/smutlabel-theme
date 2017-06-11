29a2ba55853d4b01df321c4f24ebd2ed232426e4
Philipp Sadler <philipp.sadler@gmail.com>
2017-06-11 23:42:12 +0200

    add custom section support; add huegelrock section

--- onepress-child/functions.php
+++ onepress-child/functions.php
@@ -1,11 +1,102 @@
 <?php
-add_action ( 'wp_enqueue_scripts', 'one_press_child_enqueue_styles' );
+
+/**
+ * Extend one press styles. 
+ */
 function one_press_child_enqueue_styles() {
   $parent_style = 'onepress-style';
   
   wp_enqueue_style ( $parent_style, get_template_directory_uri () . '/style.css' );
-  wp_enqueue_style ( 'child-style', get_stylesheet_directory_uri () . '/style.css', array (
+  wp_enqueue_style ( 'onepress-child-style', get_stylesheet_directory_uri () . '/style.css', array (
       $parent_style 
   ), wp_get_theme ()->get ( 'Version' ) );
 }
+add_action ( 'wp_enqueue_scripts', 'one_press_child_enqueue_styles' );
+
+/**
+ * Extend one press theme section parts by allowing additional section parts.
+ *
+ * Put additional sections in the $additional array.
+ */
+function cmp_section_by_position($a, $b) {
+  return $a ["position"] - $b ["position"];
+}
+
+if (! function_exists ( "onepress_child_frontpage_section_parts" )) {
+
+  function onepress_child_frontpage_section_parts() {
+    $additionals = array (
+        array (
+            "name" => 'huegelrock',
+            "position" => 150 
+        ) 
+    );
+    
+    $originals = array (
+        array (
+            "name" => 'hero',
+            "position" => 100 
+        ),
+        array (
+            "name" => 'features',
+            "position" => 200 
+        ),
+        array (
+            "name" => 'about',
+            "position" => 300 
+        ),
+        array (
+            "name" => 'services',
+            "position" => 400 
+        ),
+        array (
+            "name" => 'videolightbox',
+            "position" => 500 
+        ),
+        array (
+            "name" => 'gallery',
+            "position" => 600 
+        ),
+        array (
+            "name" => 'counter',
+            "position" => 700 
+        ),
+        array (
+            "name" => 'team',
+            "position" => 800 
+        ),
+        array (
+            "name" => 'news',
+            "position" => 900 
+        ),
+        array (
+            "name" => 'contact',
+            "position" => 1000 
+        ) 
+    );
+    $merged_sections = array_merge ( $originals, $additionals );
+    usort ( $merged_sections, "cmp_section_by_position" );
+    $sections = apply_filters ( 'onepress_frontpage_sections_order', array_column ( $merged_sections, "name" ) );
+    
+    foreach ( $sections as $section ) {
+      /**
+       * Hook before section
+       */
+      do_action ( 'onepress_before_section_' . $section );
+      do_action ( 'onepress_before_section_part', $section );
+      
+      /**
+       * Load section template part
+       */
+      get_template_part ( 'section-parts/section', $section );
+      
+      /**
+       * Hook after section
+       */
+      do_action ( 'onepress_after_section_part', $section );
+      do_action ( 'onepress_after_section_' . $section );
+    }
+  }
+}
+add_action ( 'onepress_frontpage_section_parts', 'onepress_child_frontpage_section_parts' );
 ?>
---
+++ onepress-child/section-parts/section-huegelrock.php
@@ -0,0 +1,24 @@
+<?php
+$subtitle = '07.07. - 09.07. 2017 - The return of rock';
+$title = 'H&uuml;gelrock Open Air 2017';
+$desc = 'Auch dieses Jahr sind wir wieder mit die Hauptorganisatoren des "h&uuml;gelrock openairs". Wir freuen uns schon euch dort zu sehen!';
+?>
+
+<section id="huegelrock"
+	class="onepress_section_class section-padding onepage-section">
+
+	<div class="container">
+		<div class="section-title-area">
+                    <?php echo '<h5 class="section-subtitle">' . $subtitle . '</h5>'; ?>
+                    <?php echo '<h2 class="section-title">' . $title . '</h2>'; ?>
+                    <?php echo '<div class="section-desc">' . apply_filters ( 'the_content', wp_kses_post ( $desc ) ) . '</div>';?>
+		</div>
+		<div class="row">
+			<div id="huegelrock-button-wrapper">
+				<a id="huegelrock-button" class="btn btn-lg"
+					href="https://www.huegelrock.de/">Schaut hier vorbei !</a>
+			</div>
+		</div>
+	</div>
+	<?php do_action('onepress_section_after_inner', 'huegelrock'); ?>
+</section>
