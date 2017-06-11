b2cafa922b5fa6959f237034c000ecb1b632d2ee
Philipp Sadler <philipp.sadler@gmail.com>
2017-06-11 22:09:22 +0200

    add functions php

---
+++ onepress-child/functions.php
@@ -0,0 +1,11 @@
+<?php
+add_action ( 'wp_enqueue_scripts', 'one_press_child_enqueue_styles' );
+function one_press_child_enqueue_styles() {
+  $parent_style = 'onepress-style';
+  
+  wp_enqueue_style ( $parent_style, get_template_directory_uri () . '/style.css' );
+  wp_enqueue_style ( 'child-style', get_stylesheet_directory_uri () . '/style.css', array (
+      $parent_style 
+  ), wp_get_theme ()->get ( 'Version' ) );
+}
+?>
